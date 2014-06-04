3fc4a32f4a50331951f53a42e754eb2c42937987
Prater, Jacob B <jacobp@ufl.edu>
2014-06-04 13:39:51 -0400

    added a course class

---
+++ Git Test/UFLCourse.h
@@ -0,0 +1,13 @@
+//
+//  UFLCourse.h
+//  Git Test
+//
+//  Created by Prater, Jacob B on 6/4/14.
+//  Copyright (c) 2014 Prater, Jacob B. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UFLCourse : NSObject
+
+@end
---
+++ Git Test/UFLCourse.m
@@ -0,0 +1,13 @@
+//
+//  UFLCourse.m
+//  Git Test
+//
+//  Created by Prater, Jacob B on 6/4/14.
+//  Copyright (c) 2014 Prater, Jacob B. All rights reserved.
+//
+
+#import "UFLCourse.h"
+
+@implementation UFLCourse
+
+@end
